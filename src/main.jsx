ecd7db64c1966204ab5f407774ccbd2199c7bd8e
anthony <anthony.steffen@outlook.com.br>
2024-10-26 14:02:53 -0300

    🔧 chore: Configuring ChakraProvider and creating app directories

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+// Chakra UI imports
+import { ChakraProvider } from '@chakra-ui/react'
+import theme  from './shared/theme'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ChakraProvider resetCSS theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
