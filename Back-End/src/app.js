d060d7b064fa8164d55b7b65f8d4b61f79a46d7f
anthony <anthony.steffen@outlook.com.br>
2024-11-20 23:30:53 -0300

    ✨ feat: Controller and routes to register and login.

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -1,22 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// Middleware to parse JSON
-app.use(express.json());
-
-// Middleware to enable CORS
-app.use(cors());
-
-// Routes
+app.use(express.json()); // Middleware to parse JSON
+app.use(cors()); // Middleware to enable CORS
+app.use('/auth', authRoutes); // Middleware to use authRoutes
 
 
 // Route Test - GET
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (_req, res) => {
+//   res.send('Hello World!');
+// });
 
 
 module.exports = app;
---
+++ Back-End/src/controllers/authController.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+// Secret key for JWT
+const JWT_SECRET = process.env.JWT_SECRET || 'jwt_secret';
+
+// Registrar novo usuário
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Verificar se o email já existe
+    const existingUser = await User.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email já cadastrado' });
+    }
+
+    // Hash da senha
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Criar o usuário
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json({ message: 'Usuário registrado com sucesso', userId: newUser.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao registrar usuário' });
+  }
+};
+
+// Login
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Verificar se o email existe
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ error: 'email não cadastrado' });
+    }
+
+    // Verificar a senha
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ error: 'Senha incorreta' });
+    }
+
+    // Gerar token JWT
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ message: 'Login realizado com sucesso', token });
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao realizar login' });
+  }
+}
+
+module.exports = { registerUser, loginUser };
