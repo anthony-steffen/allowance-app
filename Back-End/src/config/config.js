59ea76c83c66bd478dd7dc0bf85bb1e1b44c1ec5
anthony <anthony.steffen@outlook.com.br>
2024-11-20 21:55:26 -0300

    ➕ feat: configuring sequelizerc and creating db

--- Back-End/src/config/config.js
+++ Back-End/src/config/config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'password',
-    database: database_development || 'dev',
+    password: process.env.DB_PASSWORD || '051211',
+    database: process.env.DB_NAME || 'dev',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
   },
