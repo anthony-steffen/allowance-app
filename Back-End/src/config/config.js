6d3e6bc127324b17eb5b2bd13dfefdb5e4ee263d
anthony <anthony.steffen@outlook.com.br>
2024-11-21 23:03:30 -0300

    ➕ chore: Creating Dockerfile

--- Back-End/src/config/config.js
+++ Back-End/src/config/config.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 
-module.exports = {
-  development: {
+const config = {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '051211',
     database: process.env.DB_NAME || 'dev',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
-  },
+  };
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
 }
+
+
