bbf865e88dab4786ff8a3bf35b4a20679ffb71e9
anthony <anthony.steffen@outlook.com.br>
2024-11-27 11:46:38 -0300

    🐛 fix: fixing status variable values

--- Back-End/src/seeders/20241121013507-default-task.js
+++ Back-End/src/seeders/20241121013507-default-task.js
@@ -10,42 +10,42 @@ module.exports = {
       description: 'Arrumar a cama todos os dias ou desarmar a rede ao acordar',
       dueDate: new Date(),
       value: 1.06,
-      status: 'Pending',
+      status: 'pending',
 
     }, {
       title: 'Escovar os dentes',
       description: ' Escovar os dentes pelo menos 2 vezes ao dia',
       dueDate: new Date(),
       value: 1,
-      status: 'Pending',
+      status: 'pending',
 
     }, {
       title: 'Lavar a louça',
       description: 'Lavar a louça após as refeições e as que por ventura sujar',
       dueDate: new Date(),
       value: 1,
-      status: 'Pending',
+      status: 'pending',
 
     }, {
       title: 'Ler 1 Capítulo da Bíblia',
       description: 'Ler ao menos 1 capítulo da Bíblia em ordem cronológica',
       dueDate: new Date(),
       value: 1,
-      status: 'Pending',
+      status: 'pending',
 
     }, {
       title: 'Tomar Banho',
       description: 'Tomar pelo menos 2 banhos',
       dueDate: new Date(),
       value: 0.5,
-      status: 'Pending',
+      status: 'pending',
 
     }, {
       title: 'Comer frutas',
       description: 'Comer pelo menos uma fruta',
       dueDate: new Date(),
       value: 0.5,
-      status: 'Pending',
+      status: 'pending',
     }
   ]);
 
